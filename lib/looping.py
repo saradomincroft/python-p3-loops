b6bf3e42ccdc154c7a598f8aeac2723d4fbdf220
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-05-17 13:43:43 +1000

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter <= 10 and counter >0 :
+        print(f"{counter}")
+        counter -= 1
+    print("Happy New Year!")
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    print([x ** 2 for x in int_list])
+    return [x ** 2 for x in int_list]
+    
+square_integers([1, 2, 3, 4, 5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+fizzbuzz()
+
